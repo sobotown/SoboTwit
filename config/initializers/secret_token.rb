673e7879e6ca09d79fc263c9ad6cf847cb9d94d1
stevesobo <stevesobo@yahoo.com>
2013-10-27 16:36:52 -0400

    sobo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoboTwit::Application.config.secret_key_base = '3a228b8f86d83d6aa331956478d18c4b8f8f0fbd784592bc2d0a9274c9bb6d3c9ab780903b988463d1bf69984797b9a3671186bdf51198aeaa0fe9e9b0f776c3'
